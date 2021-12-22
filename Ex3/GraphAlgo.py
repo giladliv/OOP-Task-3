9034b1c98f7c233666546b0e5f882c3a1169af02
Gilad Livshitz <giladliv123@gmail.com>
2021-12-22 13:24:08 +0200

    save load and center

--- Ex3/GraphAlgo.py
+++ Ex3/GraphAlgo.py
@@ -1,12 +1,70 @@
+import json
+
 from src.GraphAlgoInterface import *
 from src.GraphInterface import GraphInterface
+from DiGraph import DiGraph
 
 class GraphAlgo(GraphAlgoInterface):
-    def __init__(self, graph: GraphInterface):
-        self._g = graph
+    def __init__(self, graph : GraphInterface = None):
+        if graph is None:
+            self._g = DiGraph()
+        else:
+            self._g = graph
 
     def get_graph(self) -> GraphInterface:
         return self._g
+
+    def load_from_json(self, file_name: str) -> bool:
+        with open(file_name) as file:
+            jCont = json.load(file)
+            file.close()
+
+        self._g = DiGraph()
+        for nodeData in jCont['Nodes']:
+            id = nodeData['id']
+            if 'pos' not in nodeData:
+                pos = None
+            else:
+                pos = tuple(str(nodeData['pos']).split(','))
+            self._g.add_node(id, pos)
+        for edgesData in jCont['Edges']:
+            src = edgesData['src']
+            dest = edgesData['dest']
+            w = float(edgesData['w'])
+            self._g.add_edge(src,dest,w)
+
+    def save_to_json(self, file_name: str) -> bool:
+        nodesList = self.get_graph().get_all_v()
+        nodes = []
+        edges = []
+
+        for node in nodesList:
+            dictNode = {}
+            dictNode['id'] = node
+            dictNode['pos'] = nodesList[node]
+            nodes += [dictNode]
+
+            inEdges = self.get_graph().all_in_edges_of_node(node)
+            for src in inEdges:
+                currInEdge = {}
+                currInEdge['src'] = src
+                currInEdge['dest'] = node
+                currInEdge['w'] = inEdges[src]
+                edges += [currInEdge]
+            outEdges = self.get_graph().all_out_edges_of_node(node)
+            for dest in outEdges:
+                currOutEdges = {}
+                currOutEdges['src'] = node
+                currOutEdges['dest'] = dest
+                currOutEdges['w'] = outEdges[dest]
+        dictJson = {}
+        dictJson['Nodes'] = nodes
+        dictJson['Edges'] = edges
+        if not file_name.endswith(".json"):
+            file_name += '.json'
+        with open(file_name, 'w') as file:
+            json.dump(dictJson, file)
+            file.close()
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         Q = []
@@ -38,3 +96,21 @@ class GraphAlgo(GraphAlgoInterface):
                     prev[v] = u
 
         return float('inf'), list()
+
+    def centerPoint(self) -> (int, float):
+        nodes = self.get_graph().get_all_v()
+        Q = {}
+        for src in nodes:
+            pathMax = -1
+            for dest in nodes:
+                w, listNodes = self.shortest_path(src, dest)
+                if w != float('inf') and pathMax < w:
+                    pathMax = w
+            if pathMax >= 0:
+                Q[src] = pathMax
+                print(str(src) + " " + str(Q[src]))
+        center = min(Q, key=Q.get)
+        return center, Q[center]
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        
--- Ex3/src/main.py
+++ Ex3/src/main.py
@@ -20,7 +20,8 @@ def check():
     """
     check0()
     #check1()
-    #check2()
+    check2()
+    check3()
 
 
 def check0():
@@ -45,7 +46,7 @@ def check0():
     print(g.all_out_edges_of_node(1))
     g_algo = GraphAlgo(g)
     print(g_algo.shortest_path(0, 3))
-    g_algo.plot_graph()
+    #g_algo.plot_graph()
 
 
 def check1():
@@ -70,6 +71,7 @@ def check2():
     g_algo = GraphAlgo()
     file = '../data/A5.json'
     g_algo.load_from_json(file)
+    print(g_algo.centerPoint())
     g_algo.get_graph().remove_edge(13, 14)
     g_algo.save_to_json(file + "_edited")
     dist, path = g_algo.shortest_path(1, 7)
@@ -80,6 +82,7 @@ def check2():
     print(dist, path)
     dist, path = g_algo.shortest_path(2, 20)
     print(dist, path)
+
     print(g_algo.TSP([1, 2, 3]))
     g_algo.plot_graph()
 
