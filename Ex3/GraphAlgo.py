687c0a07ad4a3dfe60ff9279b0ba1e3e9eee547c
Gilad Livshitz <giladliv123@gmail.com>
2021-12-28 20:13:18 +0200

    currentky works the best, this is one of the lastest parts

--- Ex3/GraphAlgo.py
+++ Ex3/GraphAlgo.py
@@ -122,20 +122,16 @@ class GraphAlgo(GraphAlgoInterface):
     def centerPoint(self) -> (int, float):
         nodes = self.get_graph().get_all_v()
         Q = {}
-        count = 0
         for src in nodes:
             pathMax = -1
             for dest in nodes:
                 w, listNodes = self.shortest_path(src, dest)
-                if len(listNodes) == len(nodes):
-                    count += 1
                 if w != float('inf') and pathMax < w:
                     pathMax = w
+                elif w == float('inf'):
+                    return None, float('inf')
             if pathMax >= 0:
                 Q[src] = pathMax
-                # print(str(src) + " " + str(Q[src]))
-        if count != len(nodes):
-            return None, float('inf')
         center = min(Q, key=Q.get)
         return center, Q[center]
 
